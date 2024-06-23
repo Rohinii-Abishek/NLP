6eb12c38a3f1bb4827da090ac34670aa446c73b9
Rohinii-Abishek <rohinii.abishek@gmail.com>
2024-06-23 14:04:27 +0530

    Add files via upload

---
+++ Stemming&Lemmatization_for_words.py
@@ -0,0 +1,20 @@
+
+from nltk.stem import PorterStemmer
+import spacy 
+
+# Initialize Stemmer
+stemmer = PorterStemmer()
+nlp = spacy.load('en_core_web_sm')
+
+# List of words
+words = ["Cook", "Boiling", "Chopped", "Frying", "Baked", "Preparation"]
+
+# Apply Stemming and Lemmatization
+for word in words:
+    stemmed_words = [stemmer.stem(word)]
+    print("stemmed_words :",stemmed_words)
+
+for word in words:
+    doc = nlp(word)
+    lemma = doc[0].lemma_
+    print("Lemmatized_words : ",lemma)
